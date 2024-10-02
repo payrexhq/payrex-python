82187389d526a69379fb5184a71c3aa292b4d722
Jaime Hing III <jaimehing@gmail.com>
2024-10-02 23:52:59 +0800

    Update version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='payrex-python',
-    version='1.0.0',
+    version='1.0.1',
     author='PayRex',
     author_email='support@payrexhq.com',
     description='PayRex Python Library',
