92ac27bcaf6fe846111af9f022e95d6ab052a045
Angelo Fallaria <ba.fallaria@gmail.com>
2025-09-27 23:58:59 +0800

    feat: add type hints for `CheckoutSessions`

--- payrex/entities/checkout_session_entity.py
+++ payrex/entities/checkout_session_entity.py
@@ -1,23 +1,38 @@
+from typing import Any, Literal, Optional, TypedDict
+from typing_extensions import NotRequired
+from payrex.type_defs import Currency, PaymentMethod
+
+
 class CheckoutSessionEntity:
     def __init__(self, api_resource):
         data = api_resource.data
 
-        self.id = data.get('id')
-        self.billing_details_collection = data.get('billing_details_collection')
-        self.customer_reference_id = data.get('customer_reference_id')
-        self.client_secret = data.get('client_secret')
-        self.status = data.get('status')
-        self.currency = data.get('currency')
-        self.line_items = data.get('line_items')
-        self.livemode = data.get('livemode')
-        self.url = data.get('url')
-        self.payment_intent = data.get('payment_intent')
-        self.metadata = data.get('metadata')
-        self.success_url = data.get('success_url')
-        self.cancel_url = data.get('cancel_url')
-        self.payment_methods = data.get('payment_methods')
-        self.description = data.get('description')
-        self.submit_type = data.get('submit_type')
-        self.expires_at = data.get('expires_at')
-        self.created_at = data.get('created_at')
-        self.updated_at = data.get('updated_at')
+        self.id: str = data.get('id')
+        self.billing_details_collection: str = data.get('billing_details_collection')
+        self.customer_reference_id: Optional[str] = data.get('customer_reference_id')
+        self.client_secret: str = data.get('client_secret')
+        self.status: Literal["active", "completed", "expired"] = data.get('status')
+        self.currency: Currency = data.get('currency')
+        self.line_items: list[LineItem] = data.get('line_items')
+        self.livemode: bool = data.get('livemode')
+        self.url: str = data.get('url')
+        self.payment_intent: dict[str, Any] = data.get('payment_intent') # TODO: payment_intent type hint
+        self.metadata: dict[str, str] = data.get('metadata')
+        self.success_url: str = data.get('success_url')
+        self.cancel_url: str = data.get('cancel_url')
+        self.payment_methods: list[PaymentMethod] = data.get('payment_methods')
+        self.description: Optional[str] = data.get('description')
+        self.submit_type: str = data.get('submit_type')
+        self.expires_at: int = data.get('expires_at')
+        self.created_at: int = data.get('created_at')
+        self.updated_at: int = data.get('updated_at')
+
+
+class LineItem(TypedDict):
+    id: str
+    resource: Literal["checkout_session_line_item"]
+    name: str
+    amount: int
+    quantity: int
+    description: NotRequired[str]
+    image: NotRequired[str]
--- payrex/services/checkout_session_service.py
+++ payrex/services/checkout_session_service.py
@@ -1,5 +1,9 @@
+from typing import TypedDict
+from typing_extensions import NotRequired
 from payrex import BaseService
 from payrex import CheckoutSessionEntity
+from payrex.entities.listing_entity import ListingEntity
+from payrex.type_defs import Currency, PaymentMethod, PaymentMethodOptionsPayload
 
 class CheckoutSessionService(BaseService):
     PATH = 'checkout_sessions'
@@ -7,7 +11,7 @@ class CheckoutSessionService(BaseService):
     def __init__(self, client):
         BaseService.__init__(self, client)
 
-    def create(self, payload):
+    def create(self, payload: 'CreateCheckoutSessionParams') -> CheckoutSessionEntity:
         return self.request(
             method='post',
             object=CheckoutSessionEntity,
@@ -15,7 +19,7 @@ class CheckoutSessionService(BaseService):
             payload=payload
         )
 
-    def list(self, payload = {}):
+    def list(self, payload: 'ListCheckoutSessionsParams' = {}) -> ListingEntity[CheckoutSessionEntity]:
         return self.request(
             method='get',
             object=CheckoutSessionEntity,
@@ -23,8 +27,8 @@ class CheckoutSessionService(BaseService):
             payload=payload,
             is_list=True
         )
-    
-    def retrieve(self, id):
+
+    def retrieve(self, id: str) -> CheckoutSessionEntity:
         return self.request(
             method='get',
             object=CheckoutSessionEntity,
@@ -32,10 +36,40 @@ class CheckoutSessionService(BaseService):
             payload={}
         )
 
-    def expire(self, id):
+    def expire(self, id: str) -> CheckoutSessionEntity:
         return self.request(
             method='post',
             object=CheckoutSessionEntity,
             path=f'{self.PATH}/{id}/expire',
             payload={}
         )
+
+
+class CreateCheckoutSessionParams(TypedDict):
+    customer_reference_id: NotRequired[str]
+    currency: Currency
+    line_items: 'list[NewLineItemParams]'
+    metadata: NotRequired[dict[str, str]]
+    success_url: str
+    cancel_url: str
+    expires_at: NotRequired[int]
+    payment_methods: list[PaymentMethod]
+    billing_details_collection: NotRequired[str]
+    description: NotRequired[str]
+    submit_type: NotRequired[str]
+    payment_method_options: NotRequired[PaymentMethodOptionsPayload]
+
+
+
+class NewLineItemParams(TypedDict):
+    name: str
+    amount: int
+    quantity: int
+    description: NotRequired[str]
+    image: NotRequired[str]
+
+
+class ListCheckoutSessionsParams(TypedDict):
+    # TODO: add parameters when API reference for 'GET /checkout_sessions'
+    # is implemented
+    pass
