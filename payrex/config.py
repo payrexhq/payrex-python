b1a922d5fe3f0e4f22da9b59466d85f5db1b4cd0
Angelo Fallaria <ba.fallaria@gmail.com>
2025-09-28 00:02:53 +0800

    refactor: do not specify concrete type for `Final` type hints, use inference

--- payrex/config.py
+++ payrex/config.py
@@ -2,7 +2,7 @@ from typing import Final
 
 
 class Config:
-    API_BASE_URL: Final[str] = 'https://api.payrexhq.com'
+    API_BASE_URL: Final = 'https://api.payrexhq.com'
 
     def __init__(self, api_key: str):
         self.api_base_url: str = self.API_BASE_URL
