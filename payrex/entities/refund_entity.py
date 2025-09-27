eaac9a2288ff630fcafe4e320d96c1babd5c4435
Angelo Fallaria <ba.fallaria@gmail.com>
2025-09-27 19:57:54 +0800

    revert: add back `RefundReason` type alias
    it was being imported by `refund_service.py` so had
    to re-add the type alias instead of inlining it
    in the `RefundEntity`

--- payrex/entities/refund_entity.py
+++ payrex/entities/refund_entity.py
@@ -2,7 +2,6 @@ from typing import Literal, Optional
 
 from payrex.type_defs import Currency
 
-
 class RefundEntity:
     def __init__(self, api_resource):
         data = api_resource.data
@@ -13,18 +12,21 @@ class RefundEntity:
         self.livemode: bool = data.get('livemode')
         self.status: Literal["succeeded", "failed", "pending"] = data.get('status')
         self.description: Optional[str] = data.get('description')
-        self.reason: Literal[
-            "fraudulent",
-            "requested_by_customer",
-            "product_out_of_stock",
-            "service_not_provided",
-            "product_was_damaged",
-            "service_misaligned",
-            "wrong_product_received",
-            "others",
-        ] = data.get('reason')
+        self.reason: RefundReason = data.get('reason')
         self.remarks: Optional[str] = data.get('remarks')
         self.payment_id: str = data.get('payment_id')
         self.metadata: Optional[dict[str, str]] = data.get('metadata')
         self.created_at: int = data.get('created_at')
         self.updated_at: int = data.get('updated_at')
+
+
+RefundReason = Literal[
+    "fraudulent",
+    "requested_by_customer",
+    "product_out_of_stock",
+    "service_not_provided",
+    "product_was_damaged",
+    "service_misaligned",
+    "wrong_product_received",
+    "others",
+]
