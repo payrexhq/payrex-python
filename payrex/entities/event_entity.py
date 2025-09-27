efd3e87240755d0c6e2a32e9874d0a2738c812df
Angelo Fallaria <ba.fallaria@gmail.com>
2025-09-27 21:34:21 +0800

    feat: add type hints to `Webhooks`

--- payrex/entities/event_entity.py
+++ payrex/entities/event_entity.py
@@ -1,11 +1,49 @@
+# pyright: reportExplicitAny=false
+from typing import Any, Literal, TypedDict
+from typing_extensions import NotRequired
+
+
 class EventEntity:
     def __init__(self, api_resource):
         data = api_resource.data
 
-        self.id = data.get('id')
-        self.data = data.get('data')
-        self.type = data.get('type')
-        self.pending_webhooks = data.get('pending_webhooks')
+        self.id: str = data.get('id')
+        self.data: EventData = data.get('data')
+        self.type: EventType = data.get('type')
+        self.pending_webhooks: int = data.get('pending_webhooks')
         self.previous_attributes = data.get('previous_attributes')
-        self.created_at = data.get('created_at')
-        self.updated_at = data.get('updated_at')
+        self.created_at: int = data.get('created_at')
+        self.updated_at: int = data.get('updated_at')
+
+
+class EventData(TypedDict):
+    resource: dict[str, Any]
+    previous_attributes: NotRequired[dict[str, Any]]
+
+
+EventType = Literal[
+    'billing_statement.created',
+    'billing_statement.updated',
+    'billing_statement.deleted',
+    'billing_statement.finalized',
+    'billing_statement.sent',
+    'billing_statement.marked_uncollectible',
+    'billing_statement.voided',
+    'billing_statement.paid',
+    'billing_statement.will_be_due',
+    'billing_statement.overdue',
+    'billing_statement_line_item.created',
+    'billing_statement_line_item.updated',
+    'billing_statement_line_item.deleted',
+    'checkout_session.expired',
+    'payment_intent.awaiting_capture',
+    'payment_intent.succeeded',
+    'payout.deposited',
+    'refund.created',
+    'refund.updated',
+]
+"""
+The available types of events that can be subscribed to using webhooks.
+
+API reference: https://docs.payrexhq.com/docs/api/events/event_types
+"""
--- payrex/entities/webhook_entity.py
+++ payrex/entities/webhook_entity.py
@@ -1,13 +1,18 @@
+from typing import Literal, Optional
+
+from payrex.entities.event_entity import EventType
+
+
 class WebhookEntity:
     def __init__(self, api_resource):
         data = api_resource.data
 
-        self.id = data.get('id')
-        self.secret_key = data.get('secret_key')
-        self.status = data.get('status')
-        self.description = data.get('description')
-        self.livemode = data.get('livemode')
-        self.url = data.get('url')
-        self.events = data.get('events')
-        self.created_at = data.get('created_at')
-        self.updated_at = data.get('updated_at')
+        self.id: str = data.get('id')
+        self.secret_key: str = data.get('secret_key')
+        self.status: Literal['enabled', 'disabled'] = data.get('status')
+        self.description: Optional[str] = data.get('description')
+        self.livemode: bool = data.get('livemode')
+        self.url: str = data.get('url')
+        self.events: list[EventType] = data.get('events')
+        self.created_at: int = data.get('created_at')
+        self.updated_at: int = data.get('updated_at')
