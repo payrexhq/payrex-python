f080931b90de3722924befdcf5f25b038812ef09
Angelo Fallaria <ba.fallaria@gmail.com>
2025-09-28 00:00:10 +0800

    fix: typo in `PayoutTransactionEntity.transaction_id` name

--- payrex/entities/payout_transaction_entity.py
+++ payrex/entities/payout_transaction_entity.py
@@ -9,6 +9,6 @@ class PayoutTransactionEntity:
         self.amount: int = data.get('amount')
         self.net_amount: int = data.get('net_amount')
         self.transaction_type: Literal["payment", "refund", "adjustment"] = data.get('transaction_type')
-        self.teransaction_id: str = data.get('transaction_id')
+        self.transaction_id: str = data.get('transaction_id')
         self.created_at: int = data.get('created_at')
         self.updated_at: int = data.get('updated_at')
