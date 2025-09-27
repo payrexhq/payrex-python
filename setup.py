b33f76671c0a5c5d9be212d578f87cd00a270e44
Angelo Fallaria <ba.fallaria@gmail.com>
2025-09-28 01:32:47 +0800

    docs: bump changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='payrex-python',
-    version='1.7.3',
+    version='1.8.0',
     author='PayRex',
     author_email='support@payrexhq.com',
     description='PayRex Python Library',
