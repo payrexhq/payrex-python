746702fed90ed301f749214e65fc1e92259725e9
josephisleta <jjosephisleta@gmail.com>
2026-02-25 09:10:35 +0800

    Return page_session on payment resource

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.7.4] - 2026-02-25
+
+- Return page_session on Payment resource.
+
 ## [1.7.3] - 2025-09-05
 
 - Resolve billing statement url.
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='payrex-python',
-    version='1.7.3',
+    version='1.7.4',
     author='PayRex',
     author_email='support@payrexhq.com',
     description='PayRex Python Library',
